b68a8dca639962b782be027d33d3276d4ee4a62f
JTilley24 <justin.tilley@me.com>
2014-01-08 18:25:55 -0500

    Added Service and API call

---
+++ Project 1 /src/com/jtilley/java2/CarService.java
@@ -0,0 +1,107 @@
+package com.jtilley.java2;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.concurrent.ExecutionException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.util.Log;
+
+public class CarService extends IntentService{
+	public String responseString = "";
+	public String urlString = "https://api.edmunds.com/api/vehicle/v2/makes?state=new&year=2014&view=full&fmt=json&api_key=saw2xy7wdxjqfueuxkv5hm8w";
+	public CarService(){
+		super("CarService");
+	}
+	
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		// TODO Auto-generated method stub
+		String[] results = getJSON(urlString);
+		
+		boolean flag = true;
+		Messenger messenger = (Messenger) intent.getExtras().get("messenger");
+		Message message = Message.obtain();
+		message.arg1 = Activity.RESULT_OK;
+		message.obj = flag;
+		try{
+			messenger.send(message);
+		} catch(RemoteException e){
+			Log.e("onHandelIntent", e.getMessage().toString());
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public String[] getJSON(String urlString){
+		String[] carObject = new String[] {responseString};
+		try {
+		getData data = new getData();
+		responseString = data.execute(urlString).get();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return carObject;
+		
+	}
+	
+	public static String getResponse(URL url){
+		String response = "";
+		
+		try {
+			URLConnection conn;
+			conn = url.openConnection();
+			BufferedInputStream bin = new BufferedInputStream(conn.getInputStream());
+			byte[] contextByte = new byte[1024];
+			int byteRead = 0;
+			StringBuffer responseBuffer = new StringBuffer();
+			while ((byteRead = bin.read(contextByte)) != -1){
+				response = new String(contextByte, 0, byteRead);
+				responseBuffer.append(response);
+			}
+			response = responseBuffer.toString();
+		} catch (IOException e) {
+			response = "No info \n" + e;
+			e.printStackTrace();
+		}
+		
+		return response;
+	}
+	
+	public class getData extends AsyncTask<String, Void, String>{
+		
+		protected String doInBackground(String... params){
+			String responseString = "";
+			try {
+				URL url = new URL (urlString);
+				responseString = getResponse(url);
+			} catch (Exception e) {
+				responseString = "No info";
+				e.printStackTrace();
+			}
+			return responseString;
+		}
+		
+		protected void onPostExecute(String results){
+			super.onPostExecute(results);
+		}
+	}
+	
+
+}
--- Project 1 /src/com/jtilley/java2/MainActivity.java
+++ Project 1 /src/com/jtilley/java2/MainActivity.java
@@ -1,7 +1,12 @@
 package com.jtilley.java2;
 
+
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Messenger;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,7 +15,27 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
+		
+		final Handler carsHandler = new Handler(){
+			@Override
+			public void handleMessage(Message msg) {
+				// TODO Auto-generated method stub
+				Object returnedObject = msg.obj;
+				
+				if(msg.arg1 == RESULT_OK && returnedObject != null){
+					
+				}
+				
+				super.handleMessage(msg);
+			}
+		};
+		
+		Messenger serviceMessenger = new Messenger(carsHandler);
+		
+		Intent carServiceIntent = new Intent(this, CarService.class);
+		carServiceIntent.putExtra("messenger", serviceMessenger);
+		startService(carServiceIntent);	
+;	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
